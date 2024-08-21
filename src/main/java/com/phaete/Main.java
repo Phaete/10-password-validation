7c0aaf9a26ba79cec3cfcdcccb682cfc936d7d6c
Phaete <pel4nk@gmail.com>
2024-08-21 15:52:34 +0200

    Add check for the password to contain a lower case letter, an upper case letter, a digit and a special character Add a check for the password length to be at least 8 characters Add a check for the password to be a 'common' password Add a function to generate a random secure password Add tests for all checks Add repeated test for the randomly generated password

--- src/main/java/com/phaete/Main.java
+++ src/main/java/com/phaete/Main.java
@@ -1,7 +1,126 @@
 package com.phaete;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        if (checkLength(password) && containsDigits(password) && containsLowerCase(password) && containsUpperCase(password) && !isCommonPassword(password)) {
+            System.out.println("Password is good to go.");
+        } else {
+            System.out.println("Password is bad, maybe choose another one.");
+        }
+    }
+
+    public static boolean checkLength(String password) {
+        return password.length() >= 8;
+    }
+
+    public static boolean containsDigits(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsLowerCase(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsUpperCase(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isCommonPassword(String password) {
+        String[] commonPasswords = {
+                "Password!",
+                "PassWord!",
+                "Password1!",
+                "PassWord1!",
+                "Password1!2",
+                "PassWord1!2",
+                "Password1!23",
+                "PassWord1!23",
+                "A1!b2C3§d4E5f6G7h8I9",
+                "a1!B2c3§D4e5F6g7H8",
+                "1!Q2w3§E4r",
+                "Sunsh1ne.",
+                "1l0veYou!123",
+                "B4seBall!",
+                "whatever_w/e",
+                "trustNo_1",
+                "Superman_123",
+                "1Qaz2w$x",
+                "StarWars.1"
+        };
+        for (String commonPassword : commonPasswords) {
+            if (password.equals(commonPassword)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsSpecialCharacters(String password) {
+        for (char c : password.toCharArray()) {
+            // Character is special if not a letter, digit, or whitespace
+            if (!Character.isLetter(c) && !Character.isDigit(c) && !Character.isWhitespace(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String generateSecurePassword() {
+        // Generate random number for length of the password, min 8 characters, max 24
+        int length = (int) (Math.random() * 16) + 8;
+        ArrayList<Object> password = new ArrayList<>();
+        // Add one lower case letter, one upper case letter, one digit, and one special character
+        password.add((char) ((int) (Math.random() * 26) + 97));
+        password.add((char) ((int) (Math.random() * 26) + 65));
+        password.add((char) ((int) (Math.random() * 10) + 48));
+        password.add((char) ((int) (Math.random() * 5) + 33));
+        // Add the rest of the characters in a loop
+        for (int i = 4; i < length; i++) {
+            // Generate random number for type of character
+            int type = (int) (Math.random() * 4);
+            // type = 0 -> lower case letter, type = 1 -> upper case letter, type = 2 -> digit, type = 3 -> special character
+            if (type == 0) {
+                // Generate random lower case letter using ASCII table
+                password.add((char) ((int) (Math.random() * 26) + 97));
+            } else if (type == 1) {
+                // Generate random upper case letter using ASCII table
+                password.add((char) ((int) (Math.random() * 26) + 65));
+            } else if (type == 2) {
+                // Generate random digit using ASCII table
+                password.add((char) ((int) (Math.random() * 10) + 48));
+            } else {
+                // Generate random special character
+                password.add((char) ((int) (Math.random() * 5) + 33));
+            }
+        }
+        // Shuffle the collection for random placement of all the characters
+        Collections.shuffle(password);
+
+        // Convert the collection to a string and return it
+        return String.valueOf(password);
     }
 }
---
+++ src/test/java/com/phaete/MainTest.java
@@ -0,0 +1,114 @@
+package com.phaete;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    // checkLength()
+    @Test
+    void checkLength_shouldBeTrue_onLength8() {
+        assertTrue(Main.checkLength("12345678"));
+    }
+
+    @Test
+    void checkLength_shouldBeFalse_onLength4() {
+        assertFalse(Main.checkLength("1234"));
+    }
+
+
+    // containsDigits()
+    @Test
+    void containsDigits_shouldBeTrue_onInputWithDigits() {
+        assertTrue(Main.containsDigits("qwertz78"));
+    }
+
+    @Test
+    void containsDigits_shouldBeFalse_onInputWithoutDigits() {
+        assertFalse(Main.containsDigits("qwertzuio"));
+    }
+
+    @Test
+    void containsDigits_shouldBeTrue_onInputWithOnlyDigits() {
+        assertTrue(Main.containsDigits("12345678"));
+    }
+
+
+    // containsLowerCase()
+    @Test
+    void containsLowerCase_shouldBeTrue_onInputWithLowerCase() {
+        assertTrue(Main.containsLowerCase("qwertz78"));
+    }
+
+    @Test
+    void containsLowerCase_shouldBeFalse_onInputWithoutLowerCase() {
+        assertFalse(Main.containsLowerCase("QWEQWEQWE"));
+    }
+
+    @Test
+    void containsLowerCase_shouldBeTrue_onInputWithMixedCase() {
+        assertTrue(Main.containsLowerCase("QwErTzUiO"));
+    }
+
+    @Test
+    void containsLowerCase_shouldBeFalse_onInputWithoutLetters() {
+        assertFalse(Main.containsLowerCase("12345678"));
+    }
+
+
+    // containsUpperCase()
+    @Test
+    void containsUpperCase_shouldBeTrue_onInputWithUpperCase() {
+        assertTrue(Main.containsUpperCase("QWERTZUIO"));
+    }
+
+    @Test
+    void containsUpperCase_shouldBeFalse_onInputWithoutUpperCase() {
+        assertFalse(Main.containsUpperCase("qwertyuio"));
+    }
+
+    @Test
+    void containsUpperCase_shouldBeTrue_onInputWithMixedCase() {
+        assertTrue(Main.containsUpperCase("QwErTzUiO"));
+    }
+
+    @Test
+    void containsUpperCase_shouldBeFalse_onInputWithoutLetters() {
+        assertFalse(Main.containsUpperCase("12345678"));
+    }
+
+
+    // isCommonPassword()
+    @Test
+    void isCommonPassword_shouldBeTrue_onCommonPassword() {
+        assertTrue(Main.isCommonPassword("Password!"));
+    }
+
+    @Test
+    void isCommonPassword_shouldBeFalse_onUniquePassword() {
+        assertFalse(Main.isCommonPassword("ThisIsTheFullstackJavaCourseFromNeueFischeAndThisPasswordIsExtraLong"));
+    }
+
+    @Test
+    void containsSpecialCharacters_shouldReturnTrue_onInputWithSpecialCharacters() {
+        assertTrue(Main.containsSpecialCharacters("password?"));
+    }
+
+    @Test
+    void containsSpecialCharacters_shouldReturnFalse_onInputWithoutSpecialCharacters() {
+        assertFalse(Main.containsSpecialCharacters("p1a2s3s4w5o6r7d8"));
+    }
+
+    @RepeatedTest(20)
+    void generateSecurePassword_shouldPassTests_onRandomPassword() {
+        String password = Main.generateSecurePassword();
+        assertTrue(Main.checkLength(password));
+        assertTrue(Main.containsDigits(password));
+        assertTrue(Main.containsLowerCase(password));
+        assertTrue(Main.containsUpperCase(password));
+        assertFalse(Main.isCommonPassword(password));
+        assertTrue(Main.containsSpecialCharacters(password));
+    }
+
+}
